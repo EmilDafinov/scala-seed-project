63e728e6b259929b010711c73d9042f73fc290d0
emildafinov <emil.dafinov@nue.io>
2025-04-08 11:26:46 -0400

    Build docker image with the tag chosen for deployment

--- src/main/script/buildApplicationImage.sh
+++ src/main/script/buildApplicationImage.sh
@@ -1,2 +1,4 @@
-echo "Image: [$IMAGE]"
-sbt 'set version := "1.0"' 'Docker / publishLocal'
+IMAGE_NAME_SPLIT=(${IMAGE//:/ })
+IMAGE_VERSION="${IMAGE_NAME_SPLIT[1]}"
+echo "Image: name [${IMAGE_NAME_SPLIT[0]}], tag: [$IMAGE_VERSION]"
+sbt "set version := \"$IMAGE_VERSION\"" "Docker / publishLocal"
